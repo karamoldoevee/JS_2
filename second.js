35577b864be4b8ce014fc2438425647233a126fe
Karamoldoev Emir <karamoldoevee@gmail.com>
2019-11-21 23:37:28 +0600

    Выполнено второе задание

---
+++ second.js
@@ -0,0 +1,22 @@
+let div_1 = document.getElementsByClassName('element')[0];
+let div_2 = document.getElementsByClassName('element')[1];
+let div_3 = document.getElementsByClassName('element')[2];
+let div_4 = document.getElementsByClassName('element')[3];
+let div_5 = document.getElementsByClassName('element')[4];
+let div_6 = document.getElementsByClassName('element')[5];
+
+div_1.style.color = 'red';
+div_2.style.color = 'red';
+div_3.style.color = 'red';
+div_4.style.color = 'green';
+div_5.style.color = 'green';
+div_6.style.color = 'green';
+
+console.log(div_1.innerText);
+console.log(div_2.innerText);
+console.log(div_3.innerText);
+console.log(div_4.innerText);
+console.log(div_5.innerText);
+console.log(div_6.innerText);
+
+
