49853d68c4f09e6109f678b48c7c4a3907759368
Karamoldoev Emir <karamoldoevee@gmail.com>
2019-11-21 23:37:10 +0600

    Выполнено первое задание

---
+++ first.js
@@ -0,0 +1,13 @@
+let string_1 = document.getElementById('string-3');
+let string_2 =  document.getElementById('string-5');
+let string_3 =  document.getElementById('string-2');
+let string_4 =  document.getElementById('string-6');
+let string_5 =  document.getElementById('string-4');
+let string_6 =  document.getElementById('string-1');
+
+console.log(string_1.innerText);
+console.log(string_2.innerText);
+console.log(string_3.innerText);
+console.log(string_4.innerText);
+console.log(string_5.innerText);
+console.log(string_6.innerText);
